c281fd6248c057f2840cc03f0d8ecd380ec6ed0b
Md. Minhaz Ahamed <mma.rifat66@gmail.com>
2019-12-23 23:11:41 +0600

    Empty config

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'https://localhost/pos/';
-//$config['base_url'] = 'https://pos.mma.champteks.us/';
+$config['base_url'] = 'https://';
 
 /*
   |--------------------------------------------------------------------------
